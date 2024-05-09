e28a039fee6c5e839ce1f615cd7d8921c073fb98
Carlos Salinas <salinas.carlos14@gmail.com>
2024-05-09 01:38:23 -0400

    Working on learning Springboot by following a tutorial

---
+++ database2/database2/src/main/java/com/turtle/database2/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.turtle.database2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping(path = "/hello")
+    public String helloWorld(){
+        return "Hello World!";
+    }
+}
